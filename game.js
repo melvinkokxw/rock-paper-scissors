cea9634e4fd8f535d7cfe381c09e27ff75eba924
Melvin Kok <melvinkokxw@gmail.com>
2020-03-22 11:55:34 +0800

    added function to verify and return user input

--- game.js
+++ game.js
@@ -1,4 +1,12 @@
 // This program is a simple program to randomly generate a computer choice for playing rock paper scissors
+// This program mainly serves as personal practice for using arrow function expressions
 
 userInput = 'Rock'; //input user choice here
 userInput.toLowerCase(); //convert uer input to lower case to ensure consistency
+
+const getUserChoice = userInput => {
+  if(userInput==='rock' || userInput==='paper' || userInput==='scissors')
+    return userInput;
+  else
+    console.log('Invalid input!');
+};
