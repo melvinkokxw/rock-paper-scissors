5a045ea58290053eae1b1562f8f42fede5912f47
Melvin Kok <melvinkokxw@gmail.com>
2020-03-22 11:39:45 +0800

    added user input

---
+++ game.js
@@ -0,0 +1,4 @@
+// This program is a simple program to randomly generate a computer choice for playing rock paper scissors
+
+userInput = 'Rock'; //input user choice here
+userInput.toLowerCase(); //convert uer input to lower case to ensure consistency
