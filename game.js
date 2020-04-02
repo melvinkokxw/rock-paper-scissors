3a7b55e089af19d85b9265a16f1d430fc64e8b3c
Melvin Kok <melvinkokxw@gmail.com>
2020-04-02 10:39:06 +0800

    Allow user input
    Added prompt to allow user to input choice of rock, paper or scissors.

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 // This program is a simple program to randomly generate a computer choice for playing rock paper scissors
 // This program mainly serves as personal practice for using arrow function expressions
 
-userInput = 'Rock'; //input user choice here
-userInput.toLowerCase(); //convert uer input to lower case to ensure consistency
+let userInput = prompt("Rock, Paper or Scissors?"); //allows user to input choice
+userInput.toLowerCase(); //convert user input to lower case to ensure consistency
 
 const getUserChoice = userInput => {
   if(userInput==='rock' || userInput==='paper' || userInput==='scissors' || userInput==='bomb')
